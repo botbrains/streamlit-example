f4ba6cc769229e2c585c5206555b876ce40cc9fe
Donovan Roberts <129420545+botbrains@users.noreply.github.com>
2024-01-19 17:55:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,14 @@
 import streamlit as st
 from langchain.llms import OpenAI
+
+st.title('🤖🧠')
+
+def generate_response(input_text):
+  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  st.info(llm(input_text))
+
+with st.form('my_form'):
+  text = st.text_area('Enter text:', 'Ask ChatGPT anything...')
+  submitted = st.form_submit_button('Submit')
+  if submitted and openai_api_key.startswith('sk-'):
+    generate_response(text)
