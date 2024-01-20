7b5b3a8d40b70a6ff825a53c4bd564511311021e
Donovan Roberts <129420545+botbrains@users.noreply.github.com>
2024-01-19 22:20:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from langchain.llms import OpenAI
 st.title('🤖🧠')
 
 def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=$OPENAI_API_KEY)
+  llm = OpenAI(temperature=0.7, openai_api_key='$OPENAI_API_KEY')
   st.info(llm(input_text))
 
 with st.form('my_form'):
